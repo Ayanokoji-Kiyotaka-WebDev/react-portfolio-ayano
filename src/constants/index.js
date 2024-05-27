1ecbb45a324e40c49df4ae91edcfb4a24490a08a
Ayanokoji-Kiyotaka-WebDev <kyilan.ntj@gmail.com>
2024-05-27 13:31:56 +0300

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,10 @@ import project2 from "../assets/projects/project1.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Hello! I'm [Ayano], a passionate full-stack developer with a penchant for crafting innovative web solutions. My journey into the world of web development began with a transformative experience at [Developer Instinct
+], where I immersed myself in an intensive full-stack bootcamp program..`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I'm a dedicated and enthusiastic recent graduate of a full-stack bootcamp, eager to kickstart my career in web development. During the bootcamp, I acquired hands-on experience with technologies like React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey into web development was sparked by a curiosity for how things work, and now I'm excited to apply my newfound skills to real-world projects. I thrive in collaborative environments and am driven by solving complex problems to deliver high-quality solutions. Beyond coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
